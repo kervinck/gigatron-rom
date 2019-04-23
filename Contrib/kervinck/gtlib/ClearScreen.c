3fed043beebd4fb15deb723ad05ee3100b8bb11c
Marcel van Kervinck <marcelk@bitpit.net>
2019-04-23 22:42:04 +0200

    Concept for LCC library setup (experimental)

---
+++ Contrib/kervinck/gtlib/ClearScreen.c
@@ -0,0 +1,39 @@
+
+/*----------------------------------------------------------------------+
+ |                                                                      |
+ |      ClearScreen -- Clear standard screen memory                     |
+ |                                                                      |
+ +----------------------------------------------------------------------*/
+
+/*
+ *  Synopsis:
+ *
+ *      #include "Gigatron.h"
+ *      #include "ClearScreen.h"
+ *
+ *      ClearScreen(color);
+ */
+
+void ClearScreen(byte color)
+{
+  int p = (int)screenMemory;
+
+  sysFn = SYS_VDrawBits_134;    // SYS function plots 8 pixels vertically
+  sysArgs[0] = color;           // Background color
+  sysArgs[2] = 0;               // All-zero bit pattern: only background
+
+  do {
+    do {
+      *(int*)(sysArgs+4) = p;   // Screen address
+      __syscall(203);           // == 270-134/2
+      p += 0x800;               // Step 8 pixels down
+    } while (p >= 0);
+
+    p += 0x8801;                // Step 120 pixels up, 1 pixel right
+  } while ((p & 255) != 160);   // Until reaching x position 160
+}
+
+/*----------------------------------------------------------------------+
+ |                                                                      |
+ +----------------------------------------------------------------------*/
+
