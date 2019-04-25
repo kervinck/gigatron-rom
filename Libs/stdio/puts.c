568427595eb5d07a01fae7c5634592122ec42ca9
Marcel van Kervinck <marcelk@bitpit.net>
2019-04-25 09:08:29 +0200

    puts(): Remove workaround
    Assume either:
    - sign extension is implemented
    - char is unsigned

--- Libs/stdio/puts.c
+++ Libs/stdio/puts.c
@@ -8,11 +8,8 @@
  */
 int puts(const char *s)
 {
-  // 2019-04-25 (marcelk) XXX Workaround missing asm.sext()
-  const byte *t = (const byte*) s;
-
-  for (; *t; t++)
-    putchar(*t);
+  for (; *s; s++)
+    putchar(*s);
   Newline();
 
   return 0;
