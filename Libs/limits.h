907e86a688d398cbe0bca88d7e3ff58a6788aeb9
Marcel van Kervinck <marcelk@bitpit.net>
2019-04-30 18:01:36 +0200

    limits.h: Initial version for Gigatron vCPU

---
+++ Libs/limits.h
@@ -0,0 +1,29 @@
+
+// Gigatron vCPU's unqualified characters are unsigned
+#define CHAR_BIT 8
+#define CHAR_MIN 0
+#define CHAR_MAX 255
+#define UCHAR_MAX 255
+#define SCHAR_MIN -128
+#define SCHAR_MAX  127
+
+// 16-bit shorts
+#define SHRT_MIN -32768
+#define SHRT_MAX  32767
+#define USHRT_MAX 65535U
+
+// 16-bit integers
+#define INT_MIN -32768
+#define INT_MAX  32767
+#define UINT_MAX 65535U
+
+// 2-byte long (not ANSI compliant)
+#define LONG_MIN -32768L
+#define LONG_MAX  32767L
+#define ULONG_MAX 65535UL
+
+// 2-byte long long (not ANSI compliant)
+#define LLONG_MIN -32768LL
+#define LLONG_MAX  32767LL
+#define ULLONG_MAX 65535ULL
+
