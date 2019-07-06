e3cda41abd755a34d51d16206650943b21c3cf57
Marcel van Kervinck <marcelk@bitpit.net>
2019-07-07 01:26:00 +0200

    Retry forking font_v2

--- Core/dev.py
+++ Core/dev.py
@@ -129,7 +129,7 @@ from os  import getenv
 
 from asm import *
 import gcl0x as gcl
-import font#_v3 as font
+import font_v2 as font
 
 # Pre-loading the formal interface as a way to get warnings when
 # accidently redefined with a different value
