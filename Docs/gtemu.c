6a25165bfbd9565dc3074e222784fe8a1ed90785
Andrey Nehaychik <andrey@nehaychik.name>
2019-08-15 12:46:02 +0300

    Revert changes and more correct fix

--- Docs/gtemu.c
+++ Docs/gtemu.c
@@ -94,10 +94,7 @@ int main(int argc, char *argv[])
     fprintf(stderr, "Error: failed to open ROM file\n");
     exit(EXIT_FAILURE);
   }
-  if (fread(ROM, 1, sizeof ROM, fp) != sizeof ROM) {
-    fprintf(stderr, "Error: incorrect reading ROM file\n");
-    exit(EXIT_FAILURE);
-  }
+  fread(ROM, 1, sizeof ROM, fp);  // Ignore: unused result here
   fclose(fp);
 
   int vgaX=0, vgaY=0;
@@ -109,8 +106,9 @@ int main(int argc, char *argv[])
     int hSync = (T.OUT & 0x40) - (S.OUT & 0x40); // "VGA monitor" (use simple stdout)
     int vSync = (T.OUT & 0x80) - (S.OUT & 0x80);
     if (vSync < 0) vgaY = -36; // Falling vSync edge
-    if (vgaX++ < 200) {
+    if (vgaX++ <= 200) {
       if (hSync) putchar('|');              // Visual indicator of hSync
+      else if (vgaX == 200) putchar('>');   // Too many pixels
       else if (~S.OUT & 0x80) putchar('^'); // Visualize vBlank pulse
       else putchar(32 + (S.OUT & 63));      // Plot pixel
     }
