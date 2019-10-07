34c2ba1d5a9a8bf3c925f807c9e5f67b479343b7
Marcel van Kervinck <marcelk@bitpit.net>
2019-10-07 09:38:35 +0200

    MSBASIC: Memory check, CHROUT and welcome message
    After that it runs wild

--- Contrib/kervinck/msbasic/make.sh
+++ Contrib/kervinck/msbasic/make.sh
@@ -11,14 +11,18 @@ ld65 -C $i.cfg tmp/$i.o -o tmp/$i.bin -Ln tmp/$i.lbl
 
 done
 
-# Gigatron
+# Create Gigatron include.gcl file
+(
+# Jump address
+awk '/COLD_START/{printf"_COLD_START=$%4s\n",$2}' tmp/gigatron.lbl
 
+# Hex dump
 od -v -A n -t x1 tmp/gigatron.bin |\
 fmt -1 |\
 awk -v A=0x280 'NF>0 {
  if(A%16==0)print""
  if(A%256==0)printf"\n*=$%x\n",A
  printf " #$%-2s",$1
- A++}' > include.gcl
-
-ls -l tmp/*.bin tmp/*.lst include.gcl
+ A++}'
+) > include.gcl
+ls -l include.gcl
