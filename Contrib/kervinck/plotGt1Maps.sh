379e5b5959a4d8b476e49eeab52118b22b07179c
Marcel van Kervinck <marcelk@bitpit.net>
2019-10-09 13:18:51 +0200

    plotGt1Maps.sh: Cleanup

--- Contrib/kervinck/plotGt1Maps.sh
+++ Contrib/kervinck/plotGt1Maps.sh
@@ -12,33 +12,31 @@ find . -name \*.gt1 |
  xargs -n 1 gt1dump.py |
   cut -c -54 |
    awk '
-   function copy() {
-     for (addr in f)
-      n[addr]++
-     delete f
-   }
-   /\* file:/ {
-     if (length(f)) copy()
-   }
-   /^[0-9a-f]/ {
-    addr=int("0x"$1)
-    for (i=0;i<NF-1;i++)
-     f[addr+i]++
-   }
-   END {
-    copy()
-    for (y=0;y<256;y++) {
-     for (x=0;x<256;x++) {
-      addr = y*256+x
-      if (addr in n)
-        z=n[addr]
-      else
-        z=0
-      print int(addr/256), addr%256, z
-     }
-     print""
+    function copy() {
+      if (length(f))
+       for (addr in f)
+        n[addr]++
+      delete f
     }
-   }' > segments.dat
+    /^\* file:/ {
+     copy()
+    }
+    /^[0-9a-f]/ {
+     addr=int("0x"$1)
+     for (i=0;i<NF-1;i++)
+      f[addr+i]++
+    }
+    END {
+     copy()
+     for (y=0;y<256;y++) {
+      for (x=0;x<256;x++) {
+       addr = y*256+x
+       z = addr in n ? n[addr] : 0
+       print int(addr/256), addr%256, z
+      }
+      print ""
+     }
+    }' > segments.dat
 
 gnuplot <<!
  set title 'Gigatron file segment heatmap (32K)'
@@ -64,12 +62,12 @@ gnuplot <<!
         7    1  0  0, \
         8    1  0  0)
  set cblabel 'Number of GT1 files'
- set terminal png size 1280,960
- set output 'Heatmap_32K.png'
  set cbrange [0.7:]
  set logscale cb
  set cbtics out 1,2
  set nomcbtics
+ set terminal png size 1280,960
+ set output 'Heatmap_32K.png'
  # Invalidate pages >= 128
  plot 'segments.dat' using 2:1:(\$1<128?\$3:1/0) with image
 
