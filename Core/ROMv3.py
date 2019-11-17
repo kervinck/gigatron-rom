91d1d18d8fb33df7bc4a40d459b9fde52ae49eaf
Ludovic Floch <ludovic.floch@gmx.com>
2019-11-17 19:09:28 +0100

    [WIP] Make ROMv3 works.

--- Core/ROMv3.py
+++ Core/ROMv3.py
@@ -267,16 +267,16 @@ minSYS = +999 # Smallest time slice for 'SYS'
 
 def runVcpu(n, ref, returnTo=None):
   """Run interpreter for exactly n cycles"""
-  comment = 'Run vCPU for %s cycles' % n
+  comment = 'Run vCPU for {!s} cycles'.format(n)
   if ref:
-    comment += ' (%s)' % ref
+    comment += ' ({!s})'.format(ref)
   if n % 2 != (vOverheadExt + vOverheadInt) % 2:
     nop()
     comment = C(comment)
     n -= 1
   n -= vOverheadExt + vOverheadInt
 
-  print 'runVcpu at %04x cycles %3s info %s' % (pc(), n, ref)
+  print('runVcpu at {:04x} cycles {:>3s} info {!s}'.format(pc(), n, ref))
   n -= 2*maxTicks
 
   assert n >= 0 and n % 2 == 0
@@ -790,7 +790,7 @@ if soundDiscontinuity == 2:
   C('Sound continuity')
   extra += 1
 if soundDiscontinuity > 2:
-  print "Warning: sound discontinuity not suppressed"
+  print("Warning: sound discontinuity not suppressed")
 
 runVcpu(189-74-extra, 'line0')  #74 Application cycles (scan line 0)
 
@@ -1993,7 +1993,7 @@ for ix in range(255):
     if ~ix & (1 << (n-1)):
       break
   pattern = ['x' if i<n else '1' if ix&(1<<i) else '0' for i in range(8)]
-  ld(ix>>n); C('0b%s >> %d' % (''.join(reversed(pattern)), n))
+  ld(ix>>n); C('0b{!s} >> %d'.format(''.join(reversed(pattern)), n))
 
 assert pc()&255 == 255
 bra([vTmp]);                    C('Jumps back into next page')
@@ -2007,7 +2007,7 @@ st([vTmp])                      #18
 ld([vAC])                       #19
 anda(0b11111110)                #20
 jmp(Y,AC)                       #21
-bra(255);                       C('bra $%04x' % (shiftTable+255))#22
+bra(255);                       C('bra ${:04x}'.format(shiftTable+255))#22
 label('.sysLsrw1a')
 st([vAC])                       #26
 ld([vAC+1]);                    C('Transfer bit 8')#27
@@ -2021,7 +2021,7 @@ st([vTmp])                      #34
 ld([vAC+1])                     #35
 anda(0b11111110)                #36
 jmp(Y,AC)                       #37
-bra(255);                       C('bra $%04x' % (shiftTable+255))#38
+bra(255);                       C('bra ${:04x}'.format(shiftTable+255))#38
 label('.sysLsrw1b')
 st([vAC+1])                     #42
 ld(hi('REENTER'), Y)            #43
@@ -2036,7 +2036,7 @@ ld([vAC])                       #18
 anda(0b11111100)                #19
 ora( 0b00000001)                #20
 jmp(Y,AC)                       #21
-bra(255);                       C('bra $%04x' % (shiftTable+255))#22
+bra(255);                       C('bra ${:04x}'.format(shiftTable+255))#22
 label('.sysLsrw2a')
 st([vAC])                       #26
 ld([vAC+1]);                    C('Transfer bit 8:9')#27
@@ -2054,7 +2054,7 @@ ld([vAC+1])                     #38
 anda(0b11111100)                #39
 ora( 0b00000001)                #40
 jmp(Y,AC)                       #41
-bra(255);                       C('bra $%04x' % (shiftTable+255))#42
+bra(255);                       C('bra ${:04x}'.format(shiftTable+255))#42
 label('.sysLsrw2b')
 st([vAC+1])                     #46
 ld(hi('REENTER'), Y)            #47
@@ -2069,7 +2069,7 @@ ld([vAC])                       #18
 anda(0b11111000)                #19
 ora( 0b00000011)                #20
 jmp(Y,AC)                       #21
-bra(255);                       C('bra $%04x' % (shiftTable+255))#22
+bra(255);                       C('bra ${:04x}'.format(shiftTable+255))#22
 label('.sysLsrw3a')
 st([vAC])                       #26
 ld([vAC+1]);                    C('Transfer bit 8:10')#27
@@ -2086,7 +2086,7 @@ ld([vAC+1])                     #37
 anda(0b11111000)                #38
 ora( 0b00000011)                #39
 jmp(Y,AC)                       #40
-bra(255);                       C('bra $%04x' % (shiftTable+255))#41
+bra(255);                       C('bra ${:04x}'.format(shiftTable+255))#41
 label('.sysLsrw3b')
 st([vAC+1])                     #45
 ld(-52/2)                       #46
@@ -2102,7 +2102,7 @@ ld([vAC])                       #18
 anda(0b11110000)                #19
 ora( 0b00000111)                #20
 jmp(Y,AC)                       #21
-bra(255);                       C('bra $%04x' % (shiftTable+255))#22
+bra(255);                       C('bra ${:04x}'.format(shiftTable+255))#22
 label('.sysLsrw4a')
 st([vAC])                       #26
 ld([vAC+1]);                    C('Transfer bit 8:11')#27
@@ -2118,7 +2118,7 @@ ld([vAC+1])                     #36
 anda(0b11110000)                #37
 ora( 0b00000111)                #38
 jmp(Y,AC)                       #39
-bra(255);                       C('bra $%04x' % (shiftTable+255))#40
+bra(255);                       C('bra ${:04x}'.format(shiftTable+255))#40
 label('.sysLsrw4b')
 st([vAC+1])                     #44
 ld(hi('REENTER'), Y)            #45
@@ -2133,7 +2133,7 @@ ld([vAC])                       #18
 anda(0b11100000)                #19
 ora( 0b00001111)                #20
 jmp(Y,AC)                       #21
-bra(255);                       C('bra $%04x' % (shiftTable+255))#22
+bra(255);                       C('bra ${:04x}'.format(shiftTable+255))#22
 label('.sysLsrw5a')
 st([vAC])                       #26
 ld([vAC+1]);                    C('Transfer bit 8:13')#27
@@ -2148,7 +2148,7 @@ ld([vAC+1])                     #35
 anda(0b11100000)                #36
 ora( 0b00001111)                #37
 jmp(Y,AC)                       #38
-bra(255);                       C('bra $%04x' % (shiftTable+255))#39
+bra(255);                       C('bra ${:04x}'.format(shiftTable+255))#39
 label('.sysLsrw5b')
 st([vAC+1])                     #44
 ld(-50/2)                       #45
@@ -2164,7 +2164,7 @@ ld([vAC])                       #18
 anda(0b11000000)                #19
 ora( 0b00011111)                #20
 jmp(Y,AC)                       #21
-bra(255);                       C('bra $%04x' % (shiftTable+255))#22
+bra(255);                       C('bra ${:04x}'.format(shiftTable+255))#22
 label('.sysLsrw6a')
 st([vAC])                       #26
 ld([vAC+1]);                    C('Transfer bit 8:13')#27
@@ -2178,7 +2178,7 @@ ld([vAC+1])                     #34
 anda(0b11000000)                #35
 ora( 0b00011111)                #36
 jmp(Y,AC)                       #37
-bra(255);                       C('bra $%04x' % (shiftTable+255))#38
+bra(255);                       C('bra ${:04x}'.format(shiftTable+255))#38
 label('.sysLsrw6b')
 st([vAC+1])                     #42
 ld(hi('REENTER'), Y)            #43
@@ -2199,7 +2199,7 @@ ld([vAC])                       #24
 anda(0b11110000)                #25
 ora( 0b00000111)                #26
 jmp(Y,AC)                       #27
-bra(255);                       C('bra $%04x' % (shiftTable+255))#28
+bra(255);                       C('bra ${:04x}'.format(shiftTable+255))#28
 label('.sysLsrl4')
 ora([vAC+1])                    #32
 st([vAC+1])                     #33
@@ -2236,7 +2236,7 @@ def trampoline3a():
   while pc()&255 < 128-7:
     nop()
   bra(AC)                       #18
-  C('Trampoline for page $%02x00 reading (entry)' % (pc()>>8))
+  C('Trampoline for page ${:02x}00 reading (entry)'.format(pc()>>8))
   bra(123)                      #19
   st([sysArgs+0])               #21
   ld([sysArgs+6])               #22
@@ -2249,7 +2249,7 @@ def trampoline3b():
   while pc()&255 < 256-6:
     nop()
   st([sysArgs+1])               #27
-  C('Trampoline for page $%02x00 reading (continue)' % (pc()>>8))
+  C('Trampoline for page ${:02x}00 reading (continue)'.format(pc()>>8))
   ld([sysArgs+6])               #28
   adda(2)                       #29
   ld(hi('txReturn'), Y)         #30
@@ -2349,7 +2349,7 @@ align(0x100, 0x100)
 
 label('font32up')
 for ch in range(32, 32+50):
-  comment = 'Char %s' % repr(chr(ch))
+  comment = 'Char {!s}'.format(repr(chr(ch)))
   for byte in font.font[ch-32]:
     ld(byte)
     comment = C(comment)
@@ -2362,7 +2362,7 @@ align(0x100, 0x100)
 
 label('font82up')
 for ch in range(32+50, 128):
-  comment = 'Char %s' % repr(chr(ch))
+  comment = 'Char {!s}'.format(repr(chr(ch)))
   for byte in font.font[ch-32]:
     ld(byte)
     comment = C(comment)
@@ -2386,7 +2386,7 @@ for i in range(0, 250, 2):
     key = int(round(32768 * freq / sampleRate))
     octave, note = j/12, notes[j%12]
     sharp = '-' if notes[j%12-1] != note else '#'
-    comment = '%s%s%s (%0.1f Hz)' % (note, sharp, octave, freq)
+    comment = '{!s}{!s}{!s} ({:0.1f} Hz)'.format(note, sharp, octave, freq)
   else:
     key, comment = 0, None
   ld(key&127); C(comment)
@@ -3193,7 +3193,7 @@ def importImage(rgbName, width, height, ref):
   for y in range(0, height, 2):
     for j in range(2):
       align(0x80)
-      comment = 'Pixels for %s line %s' % (ref, y+j)
+      comment = 'Pixels for {!s} line {!s}'.format(ref, y+j)
       for x in range(0, width, 4):
         bytes = []
         for i in range(4):
@@ -3321,14 +3321,14 @@ for i in xrange(len(packed)):
 #-----------------------------------------------------------------------
 
 # For info
-print 'SYS limits low %s high %s' % (repr(minSYS), repr(maxSYS))
+print('SYS limits low {!s} high {!s}'.format(repr(minSYS), repr(maxSYS)))
 
 # Export some zero page variables to GCL
 # These constants were already loaded from interface.json.
 # We're redefining them here to get a consistency check.
 define('memSize',    memSize)
 for i in range(3):
-  define('entropy%d' % i, entropy+i)
+  define('entropy%d'.format(i), entropy+i)
 define('videoY',     videoY)
 define('frameCount', frameCount)
 define('serialRaw',  serialRaw)
@@ -3342,7 +3342,7 @@ define('vSP',        vSP)
 define('romType',    romType)
 define('sysFn',      sysFn)
 for i in range(8):
-  define('sysArgs%d' % i, sysArgs+i)
+  define('sysArgs%d'.format(i), sysArgs+i)
 define('soundTimer', soundTimer)
 define('ledTimer',   ledTimer)
 define('ledState_v2',ledState_v2)
@@ -3386,7 +3386,7 @@ def basicLine(address, number, text):
 gtbName = 'Apps/TicTac/TicTac_v1.gtb'
 name = 'TicTac'
 print
-print 'Including file %s label %s ROM %04x' % (gtbName, name, pc())
+print('Including file {!s} label {!s} ROM {:04x}'.format(gtbName, name, pc()))
 label(name)
 address = 0x1bc0
 program = gcl.Program(address, name)
@@ -3407,7 +3407,7 @@ for line in open(gtbName):
     address += 160
 basicLine(address+2, None, 'RUN')       # Startup command
 basicLine(0x1ba0, address, None)        # End of program
-print ' Lines', i
+print(' Lines {}'.format(i))
 program.putInRomTable(0)
 program.end()
 
@@ -3422,7 +3422,7 @@ for application in argv[1:]:
     name, application = application.split('=', 1)
     label(name)
     print
-    print 'Labeling file %s label %s ROM %04x' % (application, name, pc())
+    print('Labeling file {!s} label {!s} ROM {:04x}'.format(application, name, pc()))
 
   # Pre-compiled GT1 files
   if application.endswith(('.gt1', '.gt1x')):
@@ -3431,12 +3431,12 @@ for application in argv[1:]:
     name = name.rsplit('_v', 1)[0]      # Remove version
     name = name.rsplit('/', 1)[-1]      # Remove path
     print
-    print 'Include file %s label %s ROM %04x' % (gt1File, name, pc())
+    print('Include file {!s} label {!s} ROM {:04x}'.format(gt1File, name, pc()))
     with open(gt1File, 'rb') as f:
       raw = f.read()
     raw = raw[:-2] # Drop start address
     if ord(raw[0]) == 0 and ord(raw[1]) + ord(raw[2]) > 0xc0:
-      print ' Warning: zero-page conflict with ROM loader (SYS_Exec_88)'
+      print(' Warning: zero-page conflict with ROM loader (SYS_Exec_88)')
     zpReset(userVars)
     label(name)
     program = gcl.Program(name)
@@ -3452,7 +3452,7 @@ for application in argv[1:]:
     name = name.rsplit('_v', 1)[0]      # Remove version
     name = name.rsplit('/', 1)[-1]      # Remove path
     print
-    print 'Compile file %s label %s ROM %04x' % (gclSource, name, pc())
+    print('Compile file {!s} label {!s} ROM {:04x}'.format(gclSource, name, pc()))
     zpReset(userVars)
     label(name)
     program = gcl.Program(name)
