2dd771facd8e64e4c705005776fd15b2ebebf142
Marcel van Kervinck <marcelk@bitpit.net>
2020-04-08 20:48:49 +0200

    Blinky2: $300 -> $200
    I was still thinking in the Apple-1 memory map

--- Apps/Blinky/Blinky2.vasm.py
+++ Apps/Blinky/Blinky2.vasm.py
@@ -3,11 +3,11 @@ from vasm import *
 
 p=0x30                          # User variables start from $30 in zero page
 
-ORG(0x300)                      # User programs can start from $300
+ORG(0x200)                      # User programs can start from $200
 -               LDWI(0x800)     # Top left pixel
 -               STW(p)          # Pointer variable p
 L('Loop');      POKE(p)         # Update pixel
 -               ADDI(1)         # Increment vAC
 -               BRA('Loop')     # Loop forever
 
-END(0x300)                      # Execution start address
+END(0x200)                      # Execution start address
