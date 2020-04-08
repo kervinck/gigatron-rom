1656986c8fa810c7d97739a90f29747a3ac6fcec
Marcel van Kervinck <marcelk@bitpit.net>
2020-04-08 15:21:24 +0200

    vasm.py: Add BYTE; Highlight errors on TTY

--- Core/vasm.py
+++ Core/vasm.py
@@ -4,25 +4,20 @@
 # This is the (back end for) a minimalistic vCPU assembler in Python.
 # Use Python itself as the front end.
 #
-# vCPU mmenomnics are upper cases, as always.
+# As always vCPU mmenomnics are upper case
 # Special words:
 #       ORG(address)    Start new segment
 #       L('Name')       Create a label
+#       BYTE(byte,...)  Insert data
 #       END(address)    Finish assembly, address is execution address
 
 import sys
 
-_gt1 = []       # [addr, [bytes ...], addr, [bytes ...], ...]
+_gt1 = []       # [addr, [byte, ...], addr, [byte, ...], ...]
+                # `byte' can be integer, symbol string, or expression tuple
 _symbols = {}   # name -> value
 
-def L(name):
-  if name in _symbols:
-    print('Error: Redefined %s' % repr(name))
-    sys.exit(1)
-  _symbols[name] = _gt1[-2] + len(_gt1[-1])
-
 def ORG(addr): _gt1.extend((addr, []))
-
 def LDWI(op):  return _emit((0x11, (_lo,op), (_hi,op)))
 def LD(op):    return _emit((0x1a, op))
 def LDW(op):   return _emit((0x21, op))
@@ -62,10 +57,12 @@ def ANDW(op):  return _emit((0xf8, op))
 def ORW(op):   return _emit((0xfa, op))
 def XORW(op):  return _emit((0xfc, op))
 def RET():     return _emit((0xff))
+def BYTE(*op): return _emit(op)
 
-def _emit(ins):
-  _gt1[-1].extend(ins)
-  return 0
+def L(name):
+  if name in _symbols:
+    error('Redefined %s' % repr(name))
+  _symbols[name] = _gt1[-2] + len(_gt1[-1])
 
 def END(start):
   with open('out.gt1', 'wb') as fp:
@@ -74,25 +71,33 @@ def END(start):
       if len(segment) == 0:
         continue
       if address + len(segment) > (address | 255) + 1:
-        print('Error: Page overrun in segment 0x%04X' % address)
-        sys.exit(1)
-      segment = [_toByte(x) for x in segment]
-      segment = [address >> 8, address & 255, len(segment) & 255] + segment
+        error('Page overrun in segment 0x%04X' % address)
+      segment = [_eval(x) for x in segment]
+      fp.write(bytes([address >> 8, address & 255, len(segment) & 255]))
       fp.write(bytes(segment))
     fp.write(bytes([0, start >> 8, start & 255]))
+
+def _emit(ins):
+  if len(_gt1) == 0:
+    error('ORG missing')
+  _gt1[-1].extend(ins)
+  return 0
 
 def _lo(x): return x & 255
 def _hi(x): return x >> 8
 def _br(x): return (x - 2) & 255
 
-def _toByte(x):
+def _eval(x):
   fn = lambda x: x
   if isinstance(x, tuple):
     fn, x = x[0], x[1]
   if isinstance(x, str):
     if x not in _symbols:
-      print('Error: Undefined %s' % repr(x))
-      sys.exit(1)
+      error('Undefined %s' % repr(x))
     x = _symbols[x]
-  return fn(x)
+  return fn(x) & 255
 
+def error(*args):
+  line = 'Error: ' + ' '.join(args)
+  print('\033[1m' + line + '\033[0m' if sys.stdout.isatty() else line)
+  sys.exit(1)
