f9040431a2a3fb4fe6fc331a65f55801128effb4
Marcel van Kervinck <marcelk@bitpit.net>
2020-04-08 10:56:59 +0200

    vasm.py: Enable pseudo-identation using unary operators

--- Apps/Blinky/Blinky2.vasm.py
+++ Apps/Blinky/Blinky2.vasm.py
@@ -1,16 +1,12 @@
-#!/usr/bin/env python3
-
 from vasm import *
 
-p=0x30
-(
-                ORG(0x300),
+p=0x30                          # User variables start from $30 in zero page
 
-                LDWI(0x800),
-                STW(p),
-L('Loop'),      POKE(p),
-                ADDI(1),
-                BRA('Loop'),
+ORG(0x300)                      # User programs can start from $300
+-               LDWI(0x800)     # Top left pixel
+-               STW(p)          # Pointer variable p
+L('Loop');      POKE(p)         # Update pixel
+-               ADDI(1)         # Increment vAC
+-               BRA('Loop')     # Loop forever
 
-                END(0x300)      # Execution address
-)
+END(0x300)                      # Execution start address
