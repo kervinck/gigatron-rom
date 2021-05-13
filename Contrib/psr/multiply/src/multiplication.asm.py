9584d831954473443c138184e1360219d680173f
Peter Russell <qwertyface@gmail.com>
2021-05-13 18:52:34 +0100

    Implement multiplication of two 7-bit numbers
    This takes 64 cycles. I can save two, at the expense of a lot of code,
    not just duplicated code, but also wasted space inside the lookup table.

--- Contrib/psr/multiply/src/multiplication.asm.py
+++ Contrib/psr/multiply/src/multiplication.asm.py
@@ -4,8 +4,11 @@ import sys
 from asm import (
     AC,
     C,
+    X,
     Y,
+    adda,
     align,
+    anda,
     bge,
     blt,
     bne,
@@ -18,24 +21,39 @@ from asm import (
     jmp,
     label,
     ld,
-    nop,
+    ora,
+    pc,
+    st,
     suba,
     writeRomFiles,
+    xora,
+    zpByte,
 )
 
 if __name__ == "__main__":
     enableListing()
 
+# Inputs
+a, b = zpByte(), zpByte()
+# Output
+result = zpByte(2)
+# Used for storage
+tmp = zpByte()
+# Used for the continuation address after lookup.
+continuation = zpByte()
+# Used for the action to take with the high byte.
+high_byte_action = zpByte()
+
+
 # The following code implements a lookup table of floored quarter squares,
 # for values up to 255.
 # This is supposed to enable a fast multiplication for 7-bit numbers.
 
-
-align(0x100, size=0x100)
 # First the high-bytes.
 # The table is shifted down by 32 places, as the first 32 high-bytes are all zero
 # This allows us to have code later in the page which we can branch back to.
 align(0x100, size=0x100)
+label("Quarter-squares lookup table")
 for i in range(32, 256):
     val = math.floor(i ** 2 / 4)
     ld(hi(val))
@@ -43,27 +61,48 @@ for i in range(32, 256):
 
 # We jump back here after looking up the low-byte of the result.
 label("low-byte return point")
-nop()
-
-label("high-byte table entry")
+ld(hi("multiply 7x7"), Y)
+jmp(Y, [continuation])
+ld(hi(pc()), Y)  # Make it easy to get back here!
+cost_of_low_byte_return = 3
+label("table entry.possibly-negative")
+# AC is negative, if b > a. Find absolute value
+blt(pc() + 3)  # 1
+bra(pc() + 3)  # 2
+suba(1)  # 3; if >= 0
+xora(0xFF)  # 3;  if < 0
+adda(1)  # 4
+cost_of_absolute = 4
+label("table entry")
 # Calculate an index into the high-byte table.
 # This is basically a matter of subtracting 32, and jumping in if the result >= 0.
 # But values greater than 160 have the sign-bit set after subtraction,
 # despite being >32.
 # We test for the sign bit and jump after subtraction even if 'negative' in these cases.
-blt(".value has high bit set")  # 1
-suba(32)  # 2
-bge(AC)  # 3
-bra("high-byte return point")  # 4
-ld(0)  # 5
-label(".value has high bit set")
-bra(AC)  # 3
-bra("high-byte return point")  # 4
-
+st([tmp])  # 1
+blt(pc() + 5)  # 2
+suba(32)  # 3
+bge(AC)  # 4
+bra([high_byte_action])  # 5
+ld(0)  # 6
+bra(AC)  # 4
+bra([high_byte_action])  # 5
+cost_of_high_byte_table_entry = 6
+# Some space here for other code?
+fillers(until=251)
 # We jump back here after looking up the high-byte of the result.
-label("high-byte return point")
-
-fillers(until=255)
+# Counting is in reverse
+label("high-byte action.invert-and-add")
+xora(0xFF)  # 4
+label("high-byte action.add")
+adda([result + 1])  # 3
+label("high-byte action.store")
+st([result + 1])  # 2
+ld([tmp])  # 1
+assert pc() & 0xFF == 0xFF, pc()
+cost_of_high_byte_invert_and_add = 4
+cost_of_high_byte_add = 3
+cost_of_high_byte_store = 2
 label("low-byte table entry")
 # With the index in the accumulator, and the current page in the Y register,
 # We jump to the right entry in the low-byte table, replace AC with the result,
@@ -75,10 +114,11 @@ label("low-byte table entry")
 # We use the Y register to return to the current page.
 # The table has two zeros at the start, meaning that we can replace the first
 # with the jump back.
-bne(AC)
+bne(AC)  # 1
 align(0x100, size=0x100)
-jmp(Y, "low-byte return point")
-ld(0)
+jmp(Y, "low-byte return point")  # 2
+ld(0)  # 3
+cost_of_low_byte_table_entry = 3
 C("0 = floor(0 ** 2 / 4) and floor(1 ** 2 / 4)")
 for i in range(2, 256):
     val = math.floor(i ** 2 / 4)
@@ -87,6 +127,76 @@ for i in range(2, 256):
 align(0x100, size=0x100)
 
 
+label("multiply 7x7")
+# The formula is floor(((a + b) ** 2) / 4) - floor(((a - b) ** 2) / 4)
+
+ld(".after-first-lookup")  # 1
+st([continuation])
+ld(hi("Quarter-squares lookup table"), Y)
+ld("high-byte action.store")
+st([high_byte_action])  # 5
+ld([a])
+jmp(Y, "table entry")  # 7
+adda([b])  # 8
+cost_to_first_lookup = 8
+
+cost_after_first_lookup = (
+    cost_to_first_lookup
+    + cost_of_high_byte_table_entry
+    + cost_of_high_byte_store
+    + cost_of_low_byte_table_entry
+    + cost_of_low_byte_return
+)
+
+label(".after-first-lookup")
+# On return we have the low-byte in the accumulator
+# We can safely add one to it without causing an overflow,
+# because 255 does not appear in the low-byte table.
+# This is part of the following subtraction.
+adda(1)  # 1
+st([result])
+ld(".after-second-lookup")
+st([continuation])
+ld("high-byte action.invert-and-add")  # 5
+st([high_byte_action])
+ld([a])
+jmp(Y, "table entry.possibly-negative")  # 8
+suba([b])  # 9
+cost_between_lookups = 9
+
+cost_after_second_lookup = (
+    cost_after_first_lookup
+    + cost_between_lookups
+    + cost_of_absolute
+    + cost_of_high_byte_table_entry
+    + cost_of_high_byte_invert_and_add
+    + cost_of_low_byte_table_entry
+    + cost_of_low_byte_return
+)
+
+label(".after-second-lookup")
+xora(0xFF)  # 1
+# We need to add this to the result
+# But we may have a carry
+adda([result])
+st([tmp])
+blt(pc() + 4)  # 5
+suba([result])  # 6
+bra(pc() + 4)  # 7
+ora([result])  # 8
+bra(pc() + 2)  # 7
+anda([result])  # 8
+anda(0b1000_0000, X)  # 9
+ld([tmp])
+st([result])  # 10
+ld([X])
+adda([result + 1])
+st([result + 1])  # 13
+cost_of_final_add = 13
+
+cost_of_7bit_multiply = cost_after_second_lookup + cost_of_final_add
+C(f"Total cost: {cost_of_7bit_multiply} cycles")
+label("done")
 end()
 
 if __name__ == "__main__":
--- Contrib/psr/multiply/test/test_multiplication.py
+++ Contrib/psr/multiply/test/test_multiplication.py
@@ -1,44 +1,117 @@
 import math
 import os.path
 import pathlib
+from types import SimpleNamespace
 
 from hypothesis import given
 from hypothesis import strategies as st
 
 import asm
-from gtemu import Emulator
+from gtemu import RAM, Emulator
 
 SRC_DIR = (pathlib.Path(__file__).parent / ".." / "src").resolve()
 SCRIPT = SRC_DIR / "multiplication.asm.py"
 
 
 def setup_module():
+    global vars
     """Load the Emulator from the multiplication script"""
     name, _ = os.path.splitext(os.path.basename(SCRIPT))
     script_globals = {"__file__": str(SCRIPT.absolute()), "__name__": name}
     with SCRIPT.open("rb") as file:
         exec(compile(file.read(), SCRIPT, "exec"), script_globals)
     Emulator.load_rom_from_asm_module()
+    vars = SimpleNamespace(**script_globals)
 
 
-@given(value=st.integers(min_value=0, max_value=255))
+_bytes = st.integers(min_value=0, max_value=255)
+
+
+@given(value=_bytes)
 def test_low_byte_lookup(value):
     """Lookup of the low-byte of a quarter square should work"""
-    Emulator.Y = asm.symbol("low-byte return point") >> 8
+    Emulator.Y = asm.symbol("Quarter-squares lookup table") >> 8
     Emulator.AC = value
     Emulator.next_instruction = "low-byte table entry"
 
-    Emulator.run_to("low-byte return point")
+    cycles = Emulator.run_to("low-byte return point")
 
     assert int(math.floor((value ** 2) / 4)) & 0xFF == Emulator.AC
+    assert vars.cost_of_low_byte_table_entry == cycles
 
 
-@given(value=st.integers(min_value=0, max_value=255))
+@given(value=_bytes)
 def test_high_byte_lookup(value):
     """Lookup of the high-byte of a quarter square should work"""
+    RAM[vars.high_byte_action] = asm.symbol("high-byte action.store")
     Emulator.AC = value
-    Emulator.next_instruction = "high-byte table entry"
+    Emulator.Y = asm.symbol("Quarter-squares lookup table") >> 8
+    Emulator.next_instruction = "table entry"
 
-    Emulator.run_to("high-byte return point")
+    cycles = Emulator.run_to("high-byte action.store")
 
     assert int(math.floor((value ** 2) / 4)) >> 8 == Emulator.AC
+    assert vars.cost_of_high_byte_table_entry == cycles
+
+
+_seven_bit_integers = st.integers(min_value=0, max_value=127)
+
+
+@given(a=_seven_bit_integers, b=_seven_bit_integers)
+def test_both_byte_lookup(a, b):
+    """Lookup of both bytes of a quarter square should work
+
+    The multiplication routine actually adds 1 to the result,
+    so storing it, so check for that.
+    """
+    RAM[vars.a] = a
+    RAM[vars.b] = b
+    Emulator.next_instruction = "multiply 7x7"
+    expected = int(math.floor(((a + b) ** 2) / 4)) + 1
+
+    cycles = Emulator.run_to(asm.symbol(".after-first-lookup") + 2)
+
+    assert expected == int.from_bytes(
+        RAM[vars.result : vars.result + 2], "little", signed=False
+    )
+    assert cycles == vars.cost_after_first_lookup + 2
+    assert asm.symbol("Quarter-squares lookup table") >> 8 == Emulator.Y
+
+
+@given(
+    a=_seven_bit_integers,
+    b=_seven_bit_integers,
+    previous_value=st.integers(min_value=0, max_value=65025),
+)
+def test_subtract_quarter_square(a, b, previous_value):
+    """to done should subtract the"""
+    RAM[vars.a] = a
+    RAM[vars.b] = b
+    RAM[vars.result : vars.result + 2] = (previous_value + 1).to_bytes(
+        2, "little", signed=False
+    )
+    Emulator.Y = asm.symbol("Quarter-squares lookup table") >> 8
+    Emulator.next_instruction = asm.symbol(".after-first-lookup") + 2
+    expected = (previous_value - math.floor((a - b) ** 2 / 4)) & 0xFFFF
+
+    cycles = Emulator.run_to(asm.symbol("done"))
+
+    assert expected == int.from_bytes(
+        RAM[vars.result : vars.result + 2], "little", signed=False
+    )
+    assert asm.symbol("Quarter-squares lookup table") >> 8 == Emulator.Y
+    assert vars.cost_of_7bit_multiply - vars.cost_after_first_lookup - 2 == cycles
+
+
+@given(a=_seven_bit_integers, b=_seven_bit_integers)
+def test_multiplication(a, b):
+    """Multiplication of two seven-bit integers should work"""
+    RAM[vars.a] = a
+    RAM[vars.b] = b
+    Emulator.next_instruction = "multiply 7x7"
+
+    cycles = Emulator.run_to("done")
+
+    result = int.from_bytes(RAM[vars.result : vars.result + 2], "little", signed=False)
+    assert a * b == result
+    assert vars.cost_of_7bit_multiply == cycles
