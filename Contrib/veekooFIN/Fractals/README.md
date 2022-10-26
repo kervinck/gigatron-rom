96faf5c0e8a030f5fed53f8edc1459662cca0163
Vesa <90590852+veekooFIN@users.noreply.github.com>
2022-10-26 09:25:22 +0300

    Add files via upload

---
+++ Contrib/veekooFIN/Fractals/README.md
@@ -0,0 +1,36 @@
+# GigatronTTL-Fractals
+Hobby code development for Gigatron TTL microcomputer - Fractals
+
+
+---------------------------------------------------------------------------------------------------------------
+Long Mandelbrot fractal set for Gigatron TTL
+
+Long Julia fractal set for Gigatron TTL
+
+Long Burning Ship fractal set for Gigatron TTL
+
+---------------------------------------------------------------------------------------------------------------
+
+
+---------------------------------------------------------------------------------------------------------------
+C language code developed for Gigatron TTL microcomputer. 
+Executable .GT1 files added.
+Runtime pictures added.
+
+---------------------------------------------------------------------------------------------------------------
+
+
+---------------------------------------------------------------------------------------------------------------
+
+Long Mandelbrot uses long integer and takes 45 minutes to draw the screen.
+
+Long Julia uses long integer and takes 35 minutes to draw the screen.
+
+Long Burning Ship uses long integer and takes 40 minutes to draw the screen
+
+Long Burning Ship zoomed uses long integer and takes 1 hour to draw the screen
+
+---------------------------------------------------------------------------------------------------------------
+Some tips in devenv101.txt
+
+
---
+++ Contrib/veekooFIN/Fractals/src/longjulia.c
@@ -0,0 +1,102 @@
+/*----------------------------------------------------------------------+
+ |                                                                      |
+ |     longjulia.c -- demonstrate fractal in gfx / quick and dirty      |
+ |                                                                      |
+ +----------------------------------------------------------------------*/
+ 
+// Standard includes
+#include <limits.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+#include <math.h>
+#include <gigatron/console.h>
+#include <gigatron/sys.h>
+
+#define WIDTH 160
+#define HEIGHT 120
+#define NORM_BITS 13
+#define F 8192
+
+int julia(long real0, long imag0) {
+  long realq, imagq; 
+  long real, imag;
+  int i;
+  long cx, cy; 
+ 
+  cx = (long) ((-0.8) * (float) F);
+  cy = (long) ((0.156) * (float) F);
+  
+  real = real0;
+  imag = imag0;
+  for (i = 0; i < 15; i++)
+  {
+    realq = (real * real) >> NORM_BITS;
+    imagq = (imag * imag) >> NORM_BITS;
+
+    if ((realq + imagq) > 32768) break;
+
+    imag = ((real * imag) >> (NORM_BITS - 1)) + cy;
+    real = realq - imagq + cx;
+  }
+  return i;
+}
+
+void drawPixel(int x, int y, int color)
+{
+  screenMemory[y][x] = color;
+}
+
+void main(void) {
+  int x, y, data;
+  int col[16];
+  long realmin, imagmin, realmax, imagmax;
+  long deltareal, deltaimag, real0, imag0;  
+  
+  SYS_SetMode(3);
+  
+  col[14] = 0x01;
+  col[13] = 0x02;
+  col[12] = 0x03;
+  col[11] = 0x07;
+  col[10] = 0x0b;
+  col[9] = 0x0f;
+  col[8] = 0x0e;
+  col[7] = 0x0d;
+  col[6] = 0x0c;
+  col[5] = 0x3c;
+  col[4] = 0x38;
+  col[3] = 0x34;
+  col[2] = 0x30;
+  col[1] = 0x20;
+  col[0] = 0x10;
+  col[15] = 0x00;
+  
+  realmin = (long) ((-2.0) * (float) F);
+  realmax = (long) ((2.0) * (float) F);
+  imagmin = (long) ((-1.2) * (float) F);
+  imagmax = (long) ((1.2) * (float) F); 
+   
+  for(y = 0; y < HEIGHT; y++ ) {
+    for(x = 0; x < WIDTH; x++ ) {
+      drawPixel(x,y,col[15]);
+    }
+  } 
+  
+  deltareal = (realmax - realmin) / (long) WIDTH;
+  deltaimag = (imagmax - imagmin) / (long) HEIGHT;
+
+  real0 = realmin; 
+  for(x = 0; x < WIDTH; x++ ) {
+    imag0 = imagmax;
+    for(y = 0; y < HEIGHT; y++ ) {
+      data = julia(real0, imag0);
+      drawPixel(x,y,col[data]);
+      imag0 -= deltaimag;
+    }
+    real0 += deltareal;
+  }
+  SYS_SetMode(0);
+}
